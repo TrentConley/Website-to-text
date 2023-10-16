023c957f3c9a4bfc6e82f361b115b0998f16061c
Trent Conley <trentconley@gmail.com>
2023-10-16 19:18:40 -0400

    extracting images work, now setting up way to read text from images

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def take_full_screenshot(url, save_path):
         
         # Press escape twice to possibly close pop-ups
         actions = ActionChains(driver)
+        time.sleep(1)
         actions.send_keys(Keys.ESCAPE).send_keys(Keys.ESCAPE).perform()
-        
         # Get scroll height
         total_height = driver.execute_script("return document.body.scrollHeight")
         
@@ -32,11 +32,16 @@ def take_full_screenshot(url, save_path):
         slices = []
         
         # Scroll and capture slices
+
         for offset in range(0, total_height, 900):  # assuming each slice captures 900 pixels
+            print(offset)
             driver.execute_script(f"window.scrollTo(0, {offset});")
             time.sleep(2)  # allow time for the page to settle
             screenshot = driver.get_screenshot_as_png()
             slices.append(Image.open(io.BytesIO(screenshot)))
+                    # Save each slice with offset number
+            slice_save_path = f"{save_path}_{offset}.png"
+            Image.open(io.BytesIO(screenshot)).save(slice_save_path)
         
         # Concatenate image slices
         screenshot = Image.new('RGB', (slices[0].width, total_height))
@@ -56,6 +61,6 @@ def take_full_screenshot(url, save_path):
         driver.quit()
 
 # Usage
-url = 'https://joyboseroy.medium.com/an-overview-of-web-page-content-extraction-5e0e2c62855d'
-save_path = 'screenshot.png'
+url = 'https://www.mamaknowsglutenfree.com/easy-gluten-free-bread'
+save_path = 'images/gluten-free-bread.png'
 take_full_screenshot(url, save_path)
