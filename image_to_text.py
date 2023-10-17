ec0ea3b3e684884c60f55c47ba770737aba357a9
Trent Conley <trentconley@gmail.com>
2023-10-17 16:05:28 -0400

    getting text from website

---
+++ image_to_text.py
@@ -0,0 +1,29 @@
+from google.cloud import vision_v1
+from google.cloud.vision_v1 import types
+import io
+import os
+
+def image_to_text(image_name, image_folder='images/', text_folder='text/'):
+    # Initialize the Vision API client
+    client = vision_v1.ImageAnnotatorClient()
+
+    # Read the image file
+    with io.open(image_folder + image_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision_v1.Image(content=content)
+
+    # Perform text detection
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    # Extracting text from the first annotation which contains the entire string
+    extracted_text = texts[0].description if texts else ''
+
+    # Create a .txt file with the same name as the image
+    txt_file_path = os.path.splitext(image_name)[0] + '.txt'
+    with open(text_folder + txt_file_path, 'w') as f:
+        f.write(extracted_text)
+
+    print(f"Text written to {txt_file_path}")
+
--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import time
 from PIL import Image
 import io
 import os
+from threading import Thread
+from image_to_text import convert_image_to_text  # replace with your actual module and function
 
-def take_full_screenshot(url, save_path):
+def take_screenshots_and_convert_to_text(url, save_path, save_folder='images', ):
     # Configure WebDriver to run headlessly
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -25,33 +27,30 @@ def take_full_screenshot(url, save_path):
         actions = ActionChains(driver)
         time.sleep(1)
         actions.send_keys(Keys.ESCAPE).send_keys(Keys.ESCAPE).perform()
+        
         # Get scroll height
         total_height = driver.execute_script("return document.body.scrollHeight")
         
-        # List to hold individual image slices
-        slices = []
-        
-        # Scroll and capture slices
+        threads = []
 
         for offset in range(0, total_height, 900):  # assuming each slice captures 900 pixels
             print(offset)
             driver.execute_script(f"window.scrollTo(0, {offset});")
             time.sleep(2)  # allow time for the page to settle
             screenshot = driver.get_screenshot_as_png()
-            slices.append(Image.open(io.BytesIO(screenshot)))
-                    # Save each slice with offset number
+            
+            # Save each slice with offset number
             slice_save_path = f"{save_path}_{offset}.png"
             Image.open(io.BytesIO(screenshot)).save(slice_save_path)
+
+            # Start a new thread to convert the image to text
+            thread = Thread(target=convert_image_to_text, args=(slice_save_path,))
+            thread.start()
+            threads.append(thread)
         
-        # Concatenate image slices
-        screenshot = Image.new('RGB', (slices[0].width, total_height))
-        offset = 0
-        for slice in slices:
-            screenshot.paste(slice, (0, offset))
-            offset += slice.height
-        
-        # Save the screenshot
-        screenshot.save(save_path)
+        # Wait for all threads to finish
+        for thread in threads:
+            thread.join()
     
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -62,5 +61,5 @@ def take_full_screenshot(url, save_path):
 
 # Usage
 url = 'https://www.mamaknowsglutenfree.com/easy-gluten-free-bread'
-save_path = 'images/gluten-free-bread.png'
-take_full_screenshot(url, save_path)
+save_path = 'images/gluten-free-bread'
+take_screenshots_and_convert_to_text(url, save_path)
